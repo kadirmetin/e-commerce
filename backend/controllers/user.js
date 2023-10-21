30db8e8fcaab5d03542131182de3065faec9d5e0
Kadir <kadir-metin@outlook.com.tr>
2023-10-21 18:03:02 +0300

    Refactor userDetail function for better error handling and security checks

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -227,7 +227,17 @@ const resetPassword = async (req, res) => {
 
 const userDetail = async (req, res, next) => {
   try {
-    const user = await User.findById(req.user._id);
+    const userId = req.user._id;
+
+    if (!userId) {
+      return res.status(400).json({ message: "Invalid user ID." });
+    }
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
 
     res.status(200).json({ user });
   } catch (error) {
